98213a1bec72974ebf1eeee975849d8696cfc083
Zahid Hasan <pzahidhasan@outlook.com>
2022-09-05 17:42:52 +0600

    database connection from dotenv type 1

--- configs/conn.js
+++ configs/conn.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
-const pool = new Pool({
-  user: "postgres",
-  password: "329DNa8nNbzPzg",
-  host: "localhost",
-  port: 5432,
-  database: "todopern_db2",
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig
+);
 
 module.exports = pool;
